da0173ef704e57bb18aeeb553882d918cfa6dc22
Ritesh <mayur.vanshkar@gmail.com>
2024-04-02 20:45:55 +0530

    Calling add with a negative number will throw an exception

--- spec/string_calculator_spec.rb
+++ spec/string_calculator_spec.rb
@@ -29,4 +29,9 @@ describe StringCalculator do
   it "supports different delimiters" do
     expect(calculator.add("//;\n1;2")).to eq(3)
   end
+
+  it "throws an exception for negative numbers" do
+    expect { calculator.add("1,-2,3,-4") }.to raise_error("negative numbers not allowed -2,-4")
+  end
+
 end
